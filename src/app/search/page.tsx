0c4ea2673ffc4eda2838a86b79f35888409c5555
Choongman Kim <cm730712@gmail.com>
2024-12-24 14:45:57 +0900

    Design: 검색 페이지 UI 작업

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,36 +1,138 @@
 import Image from "next/image";
 import SearchBar from "./_components/SearchBar";
 import bookCover from "/public/SampleCover.jpeg";
+import Bookmark from "./_components/Bookmark";
 
 export default function Search() {
   return (
-    <section className="flexCenter flex-col mt-20">
+    <section className="flexCenter mt-20 flex-col font-main">
       <SearchBar />
-      <ul className="w-6/12 border-2 border-neutral-200 rounded-lg bg-white">
-        <li>
+      <ul className="flex flex-col items-center gap-5">
+        <li className="searchList">
           <article className="flex">
-            <Image src={bookCover} width={190} height={230} alt="북커버" />
-            <div>
-              <h2 className="text-font-textPrimary text-3xl ">Why We Sleep</h2>
-              <p className="text-font-textSecondary">
-                인간은 밤을 살 수 있고, 누구나 행복해질 수 있다...
+            <Image
+              src={bookCover}
+              width={190}
+              height={230}
+              alt="북커버"
+              className="mx-8 my-3 shadow-xl"
+            />
+            <div className="border-border border"></div>
+            <div className="mx-8 my-4 flex w-7/12 flex-col gap-2">
+              <h2 className="font-styled text-3xl font-extrabold text-font-textPrimary">
+                Why We Sleep
+              </h2>
+              <p className="flex-grow font-medium text-font-textSecondary">
+                인간은 변할 수 있고, 누구나 행복해 질 수 있다. 단 그러기
+                위해서는 ‘용기’가 필요하다고 말한 철학자가 있다. 바로 프로이트,
+                융과 함께 ‘심리학의 3대 거장’으로 일컬어지고 있는 알프레드
+                아들러다
               </p>
-              <dl>
-                <div>
-                  <dt>작가</dt>
-                  <dd>360 pages, Kindle Edition</dd>
+              <dl className="grid gap-2 text-sm leading-5 text-font-textPrimary">
+                <div className="flex">
+                  <dt className="searchDetail">작가</dt>
+                  <dd className="font-semibold">360 pages, Kindle Edition</dd>
                 </div>
-                <div>
-                  <dt>번역</dt>
-                  <dd>First published September 28, 2017</dd>
+                <div className="flex">
+                  <dt className="searchDetail">번역</dt>
+                  <dd className="font-semibold">
+                    First published September 28, 2017
+                  </dd>
                 </div>
-                <div>
-                  <dt>출판사</dt>
-                  <dd>English</dd>
+                <div className="flex">
+                  <dt className="searchDetail">출판사</dt>
+                  <dd className="font-semibold">English</dd>
                 </div>
-                <div>
-                  <dt>출판 날짜</dt>
-                  <dd>August, 2024</dd>
+                <div className="flex">
+                  <dt className="searchDetail">출판 날짜</dt>
+                  <dd className="font-semibold">August, 2024</dd>
+                </div>
+              </dl>
+            </div>
+          </article>
+        </li>
+        <li className="searchList">
+          <article className="flex">
+            <Image
+              src={bookCover}
+              width={190}
+              height={230}
+              alt="북커버"
+              className="mx-8 my-3 shadow-xl"
+            />
+            <div className="border-border border"></div>
+            <div className="mx-8 my-4 flex w-7/12 flex-col gap-2">
+              <h2 className="font-styled text-3xl font-extrabold text-font-textPrimary">
+                Why We Sleep
+              </h2>
+              <p className="flex-grow font-medium text-font-textSecondary">
+                인간은 변할 수 있고, 누구나 행복해 질 수 있다. 단 그러기
+                위해서는 ‘용기’가 필요하다고 말한 철학자가 있다. 바로 프로이트,
+                융과 함께 ‘심리학의 3대 거장’으로 일컬어지고 있는 알프레드
+                아들러다
+              </p>
+              <dl className="grid gap-2 text-sm leading-5 text-font-textPrimary">
+                <div className="flex">
+                  <dt className="searchDetail">작가</dt>
+                  <dd className="font-semibold">360 pages, Kindle Edition</dd>
+                </div>
+                <div className="flex">
+                  <dt className="searchDetail">번역</dt>
+                  <dd className="font-semibold">
+                    First published September 28, 2017
+                  </dd>
+                </div>
+                <div className="flex">
+                  <dt className="searchDetail">출판사</dt>
+                  <dd className="font-semibold">English</dd>
+                </div>
+                <div className="flex">
+                  <dt className="searchDetail">출판 날짜</dt>
+                  <dd className="font-semibold">August, 2024</dd>
+                </div>
+              </dl>
+            </div>
+          </article>
+        </li>
+        <li className="searchList">
+          <article className="relative flex">
+            <Image
+              src={bookCover}
+              width={190}
+              height={230}
+              alt="북커버"
+              className="mx-8 my-3 shadow-xl"
+            />
+            <Bookmark />
+            <div className="border-border border" />
+            <div className="mx-8 my-4 flex w-7/12 flex-col gap-2">
+              <h2 className="font-styled text-3xl font-extrabold text-font-textPrimary">
+                Why We Sleep
+              </h2>
+              <p className="flex-grow font-medium text-font-textSecondary">
+                인간은 변할 수 있고, 누구나 행복해 질 수 있다. 단 그러기
+                위해서는 ‘용기’가 필요하다고 말한 철학자가 있다. 바로 프로이트,
+                융과 함께 ‘심리학의 3대 거장’으로 일컬어지고 있는 알프레드
+                아들러다
+              </p>
+              <dl className="grid gap-2 text-sm leading-5 text-font-textPrimary">
+                <div className="flex">
+                  <dt className="searchDetail">작가</dt>
+                  <dd className="font-semibold">360 pages, Kindle Edition</dd>
+                </div>
+                <div className="flex">
+                  <dt className="searchDetail">번역</dt>
+                  <dd className="font-semibold">
+                    First published September 28, 2017
+                  </dd>
+                </div>
+                <div className="flex">
+                  <dt className="searchDetail">출판사</dt>
+                  <dd className="font-semibold">English</dd>
+                </div>
+                <div className="flex">
+                  <dt className="searchDetail">출판 날짜</dt>
+                  <dd className="font-semibold">August, 2024</dd>
                 </div>
               </dl>
             </div>

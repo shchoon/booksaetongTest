3b20da8682edd36a7f9de098ec5167134f79aa60
shchoon <acckr0604@gmail.com>
2024-12-26 15:31:18 +0900

    Feat: router 기능 추가

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,16 +1,45 @@
-import Image from "next/image"
-import Logo from '/public/logo.png'
-import Searchicon from '/public/icons/Search.png'
-import BookIcon from '/public/icons/Books.png'
+"use client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import Logo from "/public/logo.png";
+import Searchicon from "/public/icons/Search.png";
+import BookIcon from "/public/icons/Books.png";
 
 export default function Navbar() {
-    return (
-        <div className="w-full h-[100px] flex items-center justify-between bg-navbar">
-            <Image src={Logo} sizes="100" alt="Logo" />
-            <div className="w-32 flex gap-5">
-                <Image src={Searchicon} sizes="28" alt="search" />
-                <Image src={BookIcon} sizes="28" alt="books" />
-            </div>
-        </div>
-    )
+  const router = useRouter();
+
+  return (
+    <div className="flex h-[100px] w-full items-center justify-between bg-navbar">
+      <Image
+        className="cursor-pointer"
+        src={Logo}
+        sizes="100"
+        alt="Logo"
+        onClick={() => {
+          router.push("/");
+        }}
+      />
+      <div className="flex w-32 gap-5">
+        <Image
+          className="cursor-pointer"
+          src={Searchicon}
+          sizes="28"
+          alt="search"
+          onClick={() => {
+            router.push("/search");
+          }}
+        />
+        <Image
+          className="cursor-pointer"
+          src={BookIcon}
+          sizes="28"
+          alt="books"
+          onClick={() => {
+            router.push("/bookshelf");
+          }}
+        />
+      </div>
+    </div>
+  );
 }
--- src/app/components/home/Search.tsx
+++ src/app/components/home/Search.tsx
@@ -1,11 +1,19 @@
 "use client";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 import SearchWhite from "/public/icons/SearchWhite.png";
 
 export default function Search() {
+  const router = useRouter();
+
   return (
-    <div className="flex size-[150px] flex-col items-center justify-center rounded-full bg-black">
+    <div
+      className="flex size-[150px] cursor-pointer flex-col items-center justify-center rounded-full bg-black"
+      onClick={() => {
+        router.push("/search");
+      }}
+    >
       <Image src={SearchWhite} width={58} height={51} alt="Go to search" />
       <span className="text-white">Go to search</span>
     </div>

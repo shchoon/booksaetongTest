3b04aa629beea4aa9a94f14b605400386cddd6ac
shchoon <acckr0604@gmail.com>
2025-01-09 12:07:59 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,6 @@ export default async function Home() {
   const randomIndex = Math.floor(Math.random() * 15);
   console.log(randomIndex);
   const data = await Test(randomIndex);
-  // useEffect(() => {
-  //   Test(randomIndex);
-  // }, []);
   console.log(data.length);
   return (
     <main className="mt-[121px] flex w-full flex-col items-center gap-4">

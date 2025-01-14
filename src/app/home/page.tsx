9d39885d01523f52c8485ce39818177e27a35253
shchoon <acckr0604@gmail.com>
2025-01-14 18:12:15 +0900

    console test

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -8,8 +8,7 @@ import { todayTopic } from "@/libs/apis/todayTopic";
 
 export default async function Home() {
   const randomIndex = RandomIndex();
-  console.log(new Date().toLocaleTimeString());
-  // const randomIndex = Math.floor(Math.random() * 15);
+  console.log("test");
 
   const data = {
     topic: await todayTopic(randomIndex),

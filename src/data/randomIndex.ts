0bb29609513110560f989858e3898632b377b9c2
shchoon <acckr0604@gmail.com>
2025-01-14 18:26:31 +0900

    console test

--- src/data/randomIndex.ts
+++ src/data/randomIndex.ts
@@ -1,13 +1,10 @@
 export default function RandomIndex() {
-  const CD = new Date().toLocaleString();
-  console.log(CD);
+  const currentMin = new Date().getMinutes();
+  console.log(currentMin);
 
-  const secOfCT = Number(CD.slice(CD.length - 2, CD.length));
-  console.log(secOfCT);
-
-  if (secOfCT > 14) {
-    return Math.floor(secOfCT / 5);
+  if (currentMin > 14) {
+    return Math.floor(currentMin / 5);
   }
 
-  return secOfCT;
+  return currentMin;
 }

752d811e47931f26cd488c5a0d4e12d6a504f8f6
shchoon <acckr0604@gmail.com>
2025-01-15 20:57:47 +0900

    vercel server time test

--- src/libs/apis/todayTopic.ts
+++ src/libs/apis/todayTopic.ts
@@ -2,15 +2,20 @@ import topics from "@/data/topics";
 
 export const todayTopic = async (randomIndex: number) => {
   const now = new Date();
+  console.log(now.toISOString());
+  now.setHours(21, 0, 0);
+  console.log(now.toISOString());
 
-  const min = now.getMinutes();
-  const comparedTime = new Date();
-  comparedTime.setHours(10, min + 3, 0);
-  console.log(Date.parse(comparedTime.toString()), Date.parse(now.toString()));
-  const revalidationTime =
-    (Date.parse(comparedTime.toString()) - Date.parse(now.toString())) / 1000;
-  console.log("comparedTime", comparedTime, "currnetTime", now);
-  console.log("revalidationTime", revalidationTime);
+  // const hour = now.getHours(),
+  //   min = now.getMinutes();
+  // const comparedTime = new Date();
+  console.log("now", now.toISOString(), "setTime", now.setHours(21, 0, 0));
+  // comparedTime.setHours(hour - 9, min + 5, 0);
+  // console.log(Date.parse(comparedTime.toString()), Date.parse(now.toString()));
+  // const revalidationTime =
+  //   (Date.parse(comparedTime.toString()) - Date.parse(now.toString())) / 1000;
+  // console.log("comparedTime", comparedTime, "currnetTime", now);
+  // console.log("revalidationTime", revalidationTime);
 
   const query = topics[randomIndex];
   const res = await fetch(
@@ -21,7 +26,7 @@ export const todayTopic = async (randomIndex: number) => {
         "Content-Type": "application/json",
         Authorization: `KakaoAK ${process.env.NEXT_PUBLIC_KAKAO_API_KEY}`,
       },
-      next: { revalidate: revalidationTime },
+      next: { revalidate: 1000 },
     },
   );
 

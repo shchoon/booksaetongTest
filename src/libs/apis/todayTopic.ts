c1d63f0d95f3d1e0686587253afff5975938f56c
shchoon <acckr0604@gmail.com>
2025-01-17 17:43:27 +0900

    test

--- src/libs/apis/todayTopic.ts
+++ src/libs/apis/todayTopic.ts
@@ -3,19 +3,19 @@ import topics from "@/data/topics";
 export const todayTopic = async (randomIndex: number) => {
   const now = new Date();
   const setTime = new Date();
-  setTime.setHours(12, 23, 0);
+  setTime.setUTCHours(24, 0, 0);
 
-  console.log(now, setTime);
-  console.log(Date.parse(now.toString()), Date.parse(setTime.toString()));
+  console.log("now", now, "setTime", setTime);
+  // console.log(Date.parse(now.toString()), Date.parse(setTime.toString()));
 
-  if (Date.parse(now.toString()) > Date.parse(setTime.toString())) {
-    setTime.setHours(36, 23, 0);
-  }
+  // if (Date.parse(now.toString()) > Date.parse(setTime.toString())) {
+  //   setTime.setHours(36, 23, 0);
+  // }
 
   const revalidationTime =
     (Date.parse(setTime.toString()) - Date.parse(now.toString())) / 1000;
 
-  console.log(revalidationTime);
+  // console.log(revalidationTime);
   // const hour = now.getHours(),
   //   min = now.getMinutes();
   // const comparedTime = new Date();
